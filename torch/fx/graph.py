1c1bca394c811538148c7a7a478fe304ddbe88b3
Horace He <horacehe2007@yahoo.com>
2021-04-30 19:07:05 -0700

    [WIP] Added skeleton of python dispatch key
    ghstack-source-id: 25b406a6359bd7439c259f030aff7f06a5f870d7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56718

--- torch/fx/graph.py
+++ torch/fx/graph.py
@@ -48,6 +48,7 @@ _register_custom_builtin('inf', 'from math import inf', math.inf)
 _register_custom_builtin('nan', 'from math import nan', math.nan)
 _register_custom_builtin('NoneType', 'NoneType = type(None)', type(None))
 _register_custom_builtin('torch', 'import torch', torch)
+_register_custom_builtin('device', 'device = torch.device', torch.device)
 
 
 def _is_magic(x: str) -> bool:
@@ -178,7 +179,11 @@ class PythonCode:
 
 
 def _format_args(args: Tuple[Argument, ...], kwargs: Dict[str, Argument]) -> str:
-    args_s = ', '.join(repr(a) for a in args)
+    def repr2(a):
+        if isinstance(a, torch.device):
+            return f'torch.{repr(a)}'
+        return repr(a)
+    args_s = ', '.join(repr2(a) for a in args)
     kwargs_s = ', '.join(f'{k} = {repr(v)}' for k, v in kwargs.items())
     if args_s and kwargs_s:
         return f'{args_s}, {kwargs_s}'
